dc781ab14c416d8fed21a7bf352076507f2f6e0f
LeePaulison <lee.paulison.jr@gmail.com>
2024-10-04 22:34:04 -0400

    Removed unneeded state.

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,15 @@
 // src/components/NavBar.jsx
-import React from "react";
-import { Flex, Heading, Spacer, Button, useDisclosure } from "@chakra-ui/react";
+import { Flex, Heading, Spacer } from "@chakra-ui/react";
 import AuthModal from "./AuthModal"; // Import the AuthModal component
 
 const NavBar = () => {
-  const [showModal, setShowModal] = React.useState(false);
 
   return (
     <Flex bg="gray.800" color="white" p={4} alignItems="center">
       <Heading size="lg">Project 404</Heading>
       <Spacer />
       {/* Include AuthModal in the NavBar */}
-      <AuthModal showModal={showModal} setShowModal={setShowModal} />
+      <AuthModal />
     </Flex>
   );
 };
